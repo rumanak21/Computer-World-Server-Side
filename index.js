2c2167f1b52110d0e808ecf2bbd5ed9d25324918
Rumana K <rumana2k20@gmail.com>
2022-05-08 18:50:29 +0600

    delete api created

--- index.js
+++ index.js
@@ -44,6 +44,14 @@ async function run(){
             res.send(result)
         } )
 
+        // Delete
+        app.delete('/product/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(query);
+            res.send(result)
+        } )
+
     }
     finally{}
 
